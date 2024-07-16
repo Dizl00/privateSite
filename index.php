9eb049674c08092367881407536501512df5522d
Dizl00 <burtsevdv2000@mail.ru>
2024-07-16 22:50:47 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+    <!-- Magnific Popup core CSS file -->
+    <!-- <link rel="stylesheet" href="/magnific-popup/magnific-popup.css"> -->
+    <!-- slick slider -->
+    <!-- <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" /> -->
+    <!-- my css -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/template.css">
+    <title></title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="all_ctn">
+            <header>
+               
+            </header>
+            <main>
+            <div class="bg-img"></div>
+            <!-- <img class="topBackground" src="images/background.jpg" alt="Background pool"> -->
+    </main>
+         </div>
+         <footer>
+        </footer>
+    </div>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<!-- jQuery Ripples JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.ripples/0.5.3/jquery.ripples.min.js"></script>
+<!-- My JS -->
+<script  src="/javascript/main.js"></script> 
+</body>
+</html>
---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".bg-img").ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.001,
+    });
+// $(".bg-img").ripples('drop', 120, 120, 10, 0.04);
+});
